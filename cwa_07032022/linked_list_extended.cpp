fdff12a2795ffea27ec4689d3b53dafe6b83da40
curaposterior <empo123@interia.pl>
2022-03-13 19:52:45 +0100

    adding some functions

--- cwa_07032022/linked_list_extended.cpp
+++ cwa_07032022/linked_list_extended.cpp
@@ -43,24 +43,43 @@ void sub_x(node* &head, int x);
 
 void sub_x(node* &head, int x);
 
+void insertAccordingly(node *&H, int val);
+
+void kinda_sort(node* &head); 
+
+void swapFirstLast(node* &head);
+
 
 int main() {
 	node* head = NULL;
 	cout << head << endl;
-	add(head, 1);
-	addToEnd(head, 2);
+
+	add(head, 2);
+	addToEnd(head, 8);
+	addToEnd(head, 1);
 	addToEnd(head, 3);
+
 	print(head);
+
+	swapFirstLast(head);
+
+	print(head);
+
+	kinda_sort(head);
+
+	print(head);
+	// print(head);
+	// kinda_sort(head);
+	// print(head);
+	// copy_elems2(head);
+	// print(head);
 	
-	copy_elems2(head);
-	print(head);
+	// zamienMiejscami(head->next);
+	// print(head);
 	
-	zamienMiejscami(head->next);
-	print(head);
+	// sub_x(head, 1);
+	// print(head);
 	
-	sub_x(head, 1);
-	print(head);
-	system("PAUSE");
 	return 0;
 }
 
@@ -170,7 +189,6 @@ void copy_elems2(node* head) {
 	}
 }
 
-
 void zamienMiejscami(node* &head) {
 	//zamiana drugiego z pierwszym
 	if (head && head->next) {
@@ -195,4 +213,56 @@ void sub_x(node* &head, int x) { //zamiana elementu x ze swoim nastepnikiem
 				zamienMiejscami(p->next);
 		}
 	}
+}
+
+void insertAccordingly(node *&H, int val) { //dzieki kuba
+    node *t = new node;
+    t->next = H;
+    H = t;
+
+    node *p = H;
+    while (p->next && p->next->val < val)
+        p = p->next;
+
+    node *e = new node;
+    e->next = p->next;
+    e->val = val;
+    p->next = e;
+
+    node *d = t;
+    H = d->next;
+    delete d;
+}
+
+void kinda_sort(node* &head) {
+	node* p = head;
+	node* pom = new node;
+	pom->val = p->val;
+	pom->next = nullptr;
+	p = p->next;
+
+	while (p != nullptr) {
+		insertAccordingly(pom, p->val);
+		p = p->next;
+	}
+	head = pom;
+}
+
+void swapFirstLast(node* &head) {
+	if (head != NULL && head->next != NULL) {
+		node* p = head;
+		while(p->next->next != nullptr)
+			p = p->next;
+		
+		node* pom_head = new node;
+		pom_head->val = head->val;
+		pom_head->next = p->next;
+		p->next = pom_head;
+
+		p = p->next;
+		p->next->next = head->next;
+		head = p->next;
+		p->next = nullptr;
+	}  
+
 }
