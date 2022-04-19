a1342843af26f1a2f834c47c6112bc65ec9fd215
curaposterior <empo123@interia.pl>
2022-04-19 21:04:21 +0200

    creating general logic for comb sort (fix swap)

--- cwa_11042022/comb_sort.cpp
+++ cwa_11042022/comb_sort.cpp
@@ -1,91 +1,59 @@
 #include <iostream>
-using namespace std;
+
+using std::cout;
+using std::endl;
 
 struct node {
-	int val;
-	node* next;
+    int val;
+    node* next;
 };
 
-//comb sort
-/*
-Swapujemy kolejne elementy 
-
-k = 1.3
-*/
-
-void combSort(node* &head);
-void swapNotClose(node* p, node* e);
-void bubbleSortEffective(node* &head);
 void add(node* &head, int new_val);
 void addToEndGuard(node* &head, int new_val);
 void print(node* head);
-void combSwap(node* &head,int k);
+void bubbleSortEffective(node* &head);
 
-int main(void) {
-	node* head = nullptr;
-	int toAdd[8] = {1,5,7,-19,3,5,6,-10};
-	for (int i = 0; i < 8; i++) addToEndGuard(head, toAdd[i]);
-	print(head);
-	combSwap(head, 2);
-	print(head);
+//fix swap function (or just delete it)
+void swap(node* p, node* e);
+int len(node* head);
+void add_simplifier(node* &phead, int val);
+void delete_simplifier(node* &phead);
 
-	//system("PAUSE");
-	return 0;
+//comb sort functions:
+void combSort(node* &head);
+void combSwap(node* &head, int gap);
+
+
+
+int main() {
+    node* head = nullptr;
+    srand(time(0));
+    for (int i = 0; i < 10; i++) addToEndGuard(head, rand() % 100 + 1);
+    print(head);
+
+    combSort(head);
+
+	print(head);
+    return 0;
 }
 
 void combSort(node* &head) {
-	node* p = head;
-	int n = 0;
-	while (p != nullptr) {
-		n++;
-		p = p->next;
-	}
-
-	int k = (int)(n / 1.3);
-	// do {
-	// 	k = n / 1.3;
-	// 	combSwap(head, k);
-	// } while (k > 1);
-	while (k != 1) {
-		combSwap(head, k);
-		k = (int)(k / 1.3);
-	}
-	cout << "koniec petli" << endl;
-	// bubbleSortEffective(head);
-}	
-
-void swapNotClose(node* p, node* e) {
-	node* b = e->next->next;
-	node* a = p->next;
-	p->next = e->next;
-	e->next = a;
-	p->next->next = a->next;
-	a->next = b;
-}
-
-void combSwap(node* &head,int k) {
-	if (head == nullptr)
-		return;
-	node* first = head;
-	node* tail = head;
-	int counter = 0;
-	while (tail != nullptr && counter != k) {
-		tail = tail->next;
-		counter++;
-	}
-	cout << "wartosc taila: " << tail->val << endl;
-
-
-	while (tail != nullptr) {
-		cout << "wartosc first: " << first->val << ", wartosc tail: " << tail->val << endl;
-		if (first->val > tail->val) {
-			cout << "wartosc" << endl;
-			swapNotClose(first, tail);
+    if (head == nullptr)
+        return;
+    else if (head && head->next == nullptr)
+        return;
+    else {
+        int k = len(head) / 1.3;
+		cout << "poczatkowe k=" << k << endl;
+		while (k >= 1) {
+			cout << k << "'e wykonanie petli" << endl;
+			// combSwap(head, k);
+			k = k / 1.3;
 		}
-		tail = tail->next;
-		first = first->next;
-	}
+		bubbleSortEffective(head);
+    }
 }
+
 
 void bubbleSortEffective(node* &head) {
 	if (head == nullptr)
@@ -179,4 +147,75 @@ void print(node* head) {
 		p = p->next;
 	}
 	cout << "NULL" << endl;
+}
+
+//doesn't work like i want it to
+void swap(node* p, node* e) {
+	node* b = e->next->next;
+	node* a = p->next;
+	p->next = e->next;
+	e->next = a;
+	p->next->next = a->next;
+	a->next = b;
+}
+
+int len(node* head) {
+    int n = 0;
+    node* p = head;
+    while (p != nullptr) {
+        n++;
+        p = p->next;
+    }
+    return n;
+}
+
+void combSwap(node* &head, int gap) {
+	add_simplifier(head, 0);
+	node* left = head;
+	node* right = head;
+
+	while (true) {
+		if (gap == 1)
+			break;
+		else if (gap > 1) {
+
+			for (int i = 0; i < gap; i++) {
+				if (right->next == nullptr)
+					break;
+				right = right->next;
+			}
+			// jestem na pozycji jeden przed zmiana
+			if (left->val > right->next->val)
+				swap(left, right->next); //doesn't work (messes up pointers)
+
+			left = left->next;
+			right = right->next;
+		}
+	}
+	delete_simplifier(head);
+}
+
+void add_simplifier(node* &phead, int val) {
+	node* obj = new node;
+	obj->val = val;
+
+	if(phead == NULL) {
+		obj->next = NULL;
+		phead = obj;
+	}
+	else {
+   		obj->next = phead;
+    	phead = obj;
+	}
+}
+
+void delete_simplifier(node* &phead) {
+	if((phead != NULL)&&(phead->next != NULL)) {
+    	node* obj = phead;
+    	phead = phead->next;
+    	delete(obj);
+	}
+	else {
+		cout<< "nie ma co usuwać"<<endl;
+	}
 }
