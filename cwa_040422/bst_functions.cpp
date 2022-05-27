dc70332b4ce5be5cb8ec706aa3b235cb49f0d0c4
curaposterior <empo123@interia.pl>
2022-05-27 18:12:05 +0200

    cleaning up

--- cwa_040422/bst_functions.cpp
+++ cwa_040422/bst_functions.cpp
@@ -46,12 +46,12 @@ tree* predecessor(tree* root);
 int main(void) {
     tree* root = nullptr;
     
+    root = insert(root, 8);
     root = insert(root, 5);
-    root = insert(root, 3);
-    root = insert(root, 1);
-    root = insert(root, 4);
-    root = insert(root, 9);
-    root = insert(root, 6);
+    root = insert(root, 7);
+    root = insert(root, 10);
+    root = insert(root, 11);
+    root = insert(root, 12);
 
     // root = build123();
     printBT(root);
